3735007cbb3ac9ef9f9f6636243c729619a8a026
Vaibhav Raj <77719373+vaibhavraj8252@users.noreply.github.com>
2021-05-27 12:11:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+//Create variables here
+
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+  background("cyan")
+  drawSprites();
+  textFont("audiowide")
+  textSize(20)
+  fill("white")
+  text ("Vaibhav Raj",650,680)
+  textSize(150)
+  fill ("white")
+  stroke("red")
+  text ("Pranav",100,300)
+  
+
+}
+
+
+
